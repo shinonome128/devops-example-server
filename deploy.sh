96c1506bc472f728d0a85779df93e890ac9249e7
shinonome128 <shinonome128@gmail.com>
2018-10-16 16:57:26 +0900

    Change copy commands

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@
 # chmod 0600 id_rsa
 chmod 0600 identity
 # scp -q -o "StrictHostKeyChecking no" -i id_rsa *.php root@$REMOTE_HOST:/var/www/html/
-scp -q -o "StrictHostKeyChecking no" -i identity *.php root@$REMOTE_HOST:/var/www/html/
+# scp -q -o "StrictHostKeyChecking no" -i identity *.php root@$REMOTE_HOST:/var/www/html/
+sudo cp *.php /var/www/html/
