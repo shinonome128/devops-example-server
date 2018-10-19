e3c5d9fa613b32d2947f77d18c91dcde1a83b605
shinonome128 <shinonome128@gmail.com>
2018-10-19 10:52:52 +0000

    Add travis config

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@
 # chmod 0600 identity
 # scp -q -o "StrictHostKeyChecking no" -i id_rsa *.php root@$REMOTE_HOST:/var/www/html/
 # scp -q -o "StrictHostKeyChecking no" -i identity *.php root@$REMOTE_HOST:/var/www/html/
-sudo cp *.php /var/www/html/
+# sudo cp *.php /var/www/html/
+scp -q -o "StrictHostKeyChecking no" -i identity *.php shinonome128@$REMOTE_HOST:/var/www/html/
