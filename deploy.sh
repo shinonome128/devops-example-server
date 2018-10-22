692b91d3a83f213095a311737b7cdf6e180266ca
shinonome128 <shinonome128@gmail.com>
2018-10-22 15:10:57 +0900

    Add chmod

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # chmod 0600 id_rsa
-# chmod 0600 identity
+chmod 0600 identity
 # scp -q -o "StrictHostKeyChecking no" -i id_rsa *.php root@$REMOTE_HOST:/var/www/html/
 # scp -q -o "StrictHostKeyChecking no" -i identity *.php root@$REMOTE_HOST:/var/www/html/
 # sudo cp *.php /var/www/html/
